b081b305c2263b3928d97a472393ce2e8c17bed9
AluisioNBR <lulu70638@gmail.com>
2022-07-17 22:01:22 -0300

    bug fixed

--- pages/theme.ts
+++ pages/theme.ts
@@ -1,7 +1,7 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
 const config: ThemeConfig = {
-  initialColorMode: 'Dark',
+  initialColorMode: 'dark',
   useSystemColorMode: false,
 }
 
